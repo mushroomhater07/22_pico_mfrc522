5e8e3c85603f2c7f1ac6d17627bfd0669d54d703
hey <103824094+mushroomhater07@users.noreply.github.com>
2023-05-01 01:37:39 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+from machine import I2C, Pin, SPI
+from mfrc522 import MFRC522
+
+led = Pin(25, Pin.OUT)
+true = Pin(15, Pin.OUT)
+false = Pin(14, Pin.OUT)
+sck = Pin(6, Pin.OUT)
+mosi = Pin(7, Pin.OUT)
+miso = Pin(4, Pin.OUT)
+sda = Pin(5, Pin.OUT)
+rst = Pin(22, Pin.OUT)
+spi = SPI(0, baudrate=100000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
+
+while True:
+    led.value(1)
+    time.sleep(0.06)
+    led.value(0)
+    rdr = MFRC522(spi, sda, rst)
+    (stat, tag_type) = rdr.request(rdr.REQIDL)
+    if stat == rdr.OK:
+        (stat, raw_uid) = rdr.anticoll()
+        if stat == rdr.OK:
+            uid = ("0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+            print(uid)
